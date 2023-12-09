c98b4239fcb9fa06fc88f09549635a7ca3168c80
Raito Bezarius <masterancpp@gmail.com>
2023-12-09 03:30:36 +0100

    feat: add a basic triage view
    This enables to search through available CVE containers in the database
    using a very simple PostgreSQL TS vector search.

--- src/website/webview/urls.py
+++ src/website/webview/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, re_path
 
-from webview.views import HomeView, NixpkgsIssueView, NixpkgsIssueListView
+from webview.views import HomeView, TriageView, NixpkgsIssueView, NixpkgsIssueListView
 
 app_name = "webview"
 
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
+    path("triage/", TriageView.as_view(), name="triage_view"),
     path("issues/", NixpkgsIssueListView.as_view(), name="issue_list"),
     re_path(
         r"^issues/(?P<code>NIXPKGS-[0-9]{4}-[0-9]{4,19})$",
--- src/website/webview/views.py
+++ src/website/webview/views.py
@@ -2,10 +2,45 @@ from django.views.generic import TemplateView, DetailView, ListView
 from django.shortcuts import get_object_or_404
 
 from shared.models import NixpkgsIssue
+from shared.models.cve import Container
+
+from django.contrib.postgres.search import SearchVector
 
 
 class HomeView(TemplateView):
     template_name = "home_view.html"
+
+
+class TriageView(ListView):
+    template_name = "triage_view.html"
+    model = Container
+    paginate_by = 25
+
+    def get_queryset(self):
+        qs = (
+            Container.objects.prefetch_related("descriptions", "affected", "cve")
+            .exclude(title="")
+            .order_by("id", "-date_public")
+        )
+        search_query = self.request.GET.get("search_query")
+        if not search_query:
+            return qs.all()
+        else:
+            return (
+                qs.annotate(
+                    search=SearchVector(
+                        "title",
+                        "descriptions__value",
+                        "affected__vendor",
+                        "affected__product",
+                        "affected__package_name",
+                        "affected__repo",
+                        "affected__cpes__name",
+                    )
+                )
+                .filter(search=search_query)
+                .distinct("id")
+            )
 
 
 class NixpkgsIssueView(DetailView):
